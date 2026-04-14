7ea164019f4677396687ee42ca85696bf4d05b8f
nastyshaaaaa <rabcikova843@gmail.com>
2026-04-14 23:16:56 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,19 @@
 import pytest
-from client_manager import ClientManager   # пока нет, напишем позже
+from client_manager import ClientManager
+from order_manager import OrderManager
 
 @pytest.fixture
 def client_manager():
-    """Фикстура, возвращающая пустой ClientManager."""
+    """Фикстура: пустой ClientManager."""
     return ClientManager()
 
 @pytest.fixture
+def order_manager():
+    """Фикстура: пустой OrderManager."""
+    return OrderManager()
+
+@pytest.fixture
 def manager_with_one_client(client_manager):
-    """Фикстура с одним предзаполненным клиентом."""
+    """ClientManager с одним клиентом."""
     client_manager.add_client("Иван Петров", "ivan@example.com")
     return client_manager
